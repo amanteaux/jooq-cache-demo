e259c692ea6f94a9b5336c2e93787d20e73fb456
Aurélien Manteaux <amanteaux@cardiweb.com>
2014-03-19 20:43:53 +0100

    Concurrent benchmark added

--- src/main/java/org/jooq/demo/MicroBenchmark.java
+++ src/main/java/org/jooq/demo/MicroBenchmark.java
@@ -1,5 +1,6 @@
 package org.jooq.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.jooq.demo.db.beans.User;
@@ -16,6 +17,9 @@ public class MicroBenchmark {
 		
 		System.out.println("thousand rows result query benchmark");
 		thousandRowsResult();
+		
+		System.out.println("one row query with 10 threads benchmark");
+		onRowResultHundredThreads();
 	}
 
 	private static void noRowResult() {
@@ -94,6 +98,54 @@ public class MicroBenchmark {
 				System.out.println( i + " query results fetched from cache");
 			}
 		}
+		long end = System.currentTimeMillis();
+		System.out.println("Total time : " + (end -start));
+	}
+	
+	private static void onRowResultHundredThreads() {
+		App app = new App();
+        final UserService userService = app.injector().getInstance(UserService.class);
+        
+        userService.create("Demo", "jOOQ", "demo@jooq.org", "jooq", "demo");
+
+        // warm up
+		for(int i=0; i<20000;i++) {
+			userService.search(null, null);
+		}
+		
+		// threads preparation
+		List<Thread> threads = new ArrayList<>();
+		for(int i=0; i<100; i++) {
+			threads.add(new Thread() {
+				@Override
+				public void run() {
+					for(int i=0; i<20000;i++) {
+						List<User> users = userService.search(null, null);
+						if(users.size() != 1 || !"demo@jooq.org".equals(users.get(0).getEmail())) {
+							throw new RuntimeException("Oops");
+						}
+						if(i%5000 == 0 && i != 0) {
+							System.out.println( i + " query results fetched from cache");
+						}
+					}
+				}
+			});
+		}
+		
+		// micro benchmark
+		long start = System.currentTimeMillis();
+		
+		for(Thread thread : threads) {
+			thread.start();
+		}
+		for(Thread thread : threads) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		
 		long end = System.currentTimeMillis();
 		System.out.println("Total time : " + (end -start));
 	}
